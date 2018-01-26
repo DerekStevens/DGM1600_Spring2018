88eb05270fd2eff46b8bd3048fce7480c07f6875
Derek Stevens <derekstevens21@gmail.com>
2018-01-25 19:25:34 -0700

    Added PlayerMove Script

--- DGM1600_Game/Assets/Scripts/CubeMove.cs
+++ DGM1600_Game/Assets/Scripts/CubeMove.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 
 public class CubeMove : MonoBehaviour {
 
-	public float transX;
-	public float transY;
-	public float transZ;
+	// public float transX;
+	// public float transY;
+	// public float transZ;
 
-	public float rotX;
-	public float rotY;
-	public float rotZ;
+	// public float rotX;
+	// public float rotY;
+	// public float rotZ;
+
+	// public float moveSpeed;
+	// public float turnSpeed;
+	// public float jumpHeight;
+
+		// public Vector3 move;
+		// public Vector3 rotate;
+		public float moveSpeed;
 
 	// Use this for initialization
 	void Start () {
@@ -19,7 +27,30 @@ public class CubeMove : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		transform.Translate(transX,transY,transZ);
-		transform.Rotate(rotX,rotY,rotZ);
+		// var j = Input.GetAxis("Jump")* Time.deltaTime * jumpHeight;
+		// var y = Input.GetAxis("Horizonal")* Time.deltaTime * turnSpeed;
+		// var z = Input.GetAxis("Vertical")* Time.deltaTime * moveSpeed;
+
+		//	transform.Translate(transX,transY,transZ);
+		//	transform.Rotate(rotX,rotY,rotZ);
+		if(Input.GetKey(KeyCode.W)){
+			transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+		}
+
+		if(Input.GetKey(KeyCode.S)){
+			transform.Translate(Vector3.back * moveSpeed * Time.deltaTime);
+		}
+
+		if(Input.GetKey(KeyCode.A)){
+			transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+		}
+
+		if(Input.GetKey(KeyCode.D)){
+			transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+		}
+		
+
+	// transform.Translate(move * Time.deltaTime);
+	// transform.Rotate(rotate * Time.deltaTime);
 	}
 }
